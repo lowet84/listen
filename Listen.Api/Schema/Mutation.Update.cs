d01365b1bd8f4b8c0c2828f9b3600a31e441cffd
Fredrik Löwenhamn <lowet84@gmail.com>
2017-09-19 09:39:11 +0200

    Wip

--- Listen.Api/Schema/Mutation.Update.cs
+++ Listen.Api/Schema/Mutation.Update.cs
@@ -14,9 +14,8 @@ namespace Listen.Api.Schema
     public partial class Mutation
     {
         [Description("Find files and update file list")]
-        public DefaultResult<UpdateFileChangesOutput> UpdateFileChanges()
+        public DefaultResult<UpdateFileChangesOutput> UpdateFileChanges(UserContext context)
         {
-            var context = new UserContext();
             var oldFiles = context.Search<AudioFile>("FilePath", "", UserContext.ReadType.Shallow) ?? new AudioFile[0];
             var settings = context.Search<Settings>("Path", "", UserContext.ReadType.Shallow).FirstOrDefault();
             if (settings?.Path == null)
@@ -35,66 +34,68 @@ namespace Listen.Api.Schema
             return new DefaultResult<UpdateFileChangesOutput>(ret);
         }
 
-        public DefaultResult<LookupBooksOutput> LookupBooks()
+        private (Book, bool) LookupBook(string folder)
         {
             var context = new UserContext();
-            var audioFiles = context.Search<AudioFile>("FilePath", "", UserContext.ReadType.Shallow);
-            if (audioFiles == null) return null;
-            var folders = audioFiles.Select(d => d.Folder).Distinct().ToList();
 
-            var books = context.Search<Book>("Title", "", UserContext.ReadType.Shallow) ?? new Book[0];
-            folders = folders.Where(folder => !books.Select(book => book.Path).Contains(folder)).ToList();
+            var existingBook = context.Search<Book>("Path", folder, UserContext.ReadType.Shallow).FirstOrDefault();
+            if (existingBook != null) return (existingBook, false);
 
-            foreach (var folder in folders)
+            var folderSearchString = Path.GetFileName(folder);
+
+            var firstFile = FindAudioFiles(folder, true).First();
+            string album;
+            string title;
+            string author;
+            using (var readStream = new FileStream(firstFile.FilePath, FileMode.Open, FileAccess.Read))
             {
-                var folderSearchString = Path.GetFileName(folder);
+                var tagFile = TagLib.File.Create(new StreamFileAbstraction(firstFile.FilePath, readStream, null));
+                album = tagFile.Tag.Album;
+                author = tagFile.Tag.FirstAlbumArtist;
+                title = tagFile.Tag.Title;
+            }
+            var tagSearchString1 = $"{author} {title}";
+            var tagSearchString2 = $"{author} {album}";
 
-                var firstFile = FindAudioFiles(folder, true).First();
-                string album;
-                string title;
-                string author;
-                using (var readStream = new FileStream(firstFile.FilePath, FileMode.Open, FileAccess.Read))
+            var results = BigBookSearch.Search(folderSearchString, tagSearchString1, tagSearchString2);
+            var bestResult = results.OrderByDescending(d => d.Number).FirstOrDefault();
+            Book book;
+            if (bestResult == null || bestResult.Number < SettingsUtil.Settings.AutoMatchThreshold)
+            {
+                book = new Book(null, null, folder, MiscUtils.Get404Image(), true);
+            }
+            else
+            {
+                var existingImage =
+                    context.Search<CoverImage>("Url", bestResult?.ImageLink, UserContext.ReadType.Shallow);
+                CoverImage image;
+                if (existingImage.Length > 0)
                 {
-                    var tagFile = TagLib.File.Create(new StreamFileAbstraction(firstFile.FilePath, readStream, null));
-                    album = tagFile.Tag.Album;
-                    author = tagFile.Tag.FirstAlbumArtist;
-                    title = tagFile.Tag.Title;
-                }
-                var tagSearchString1 = $"{author} {title}";
-                var tagSearchString2 = $"{author} {album}";
-
-                var results = BigBookSearch.Search(folderSearchString, tagSearchString1, tagSearchString2);
-                var bestResult = results.OrderByDescending(d => d.Number).FirstOrDefault();
-                Book book;
-                if (bestResult == null || bestResult.Number < SettingsUtil.Settings.AutoMatchThreshold)
-                {
-                    book = new Book(null,null,folder, MiscUtils.Get404Image(), true);
+                    image = existingImage.First();
                 }
                 else
                 {
-                    var existing =
-                        context.Search<CoverImage>("Url", bestResult?.ImageLink, UserContext.ReadType.Shallow);
-                    CoverImage image;
-                    if (existing.Length > 0)
-                    {
-                        image = existing.First();
-                    }
-                    else
-                    {
-                        image = new CoverImage(bestResult?.ImageLink, MiscUtils.DownloadImage(bestResult?.ImageLink));
-                        context.AddDefault(image);
-                    }
-
-                    book = new Book(bestResult?.Title, bestResult?.Author, folder, image, false);
+                    image = new CoverImage(bestResult?.ImageLink, MiscUtils.DownloadImage(bestResult?.ImageLink));
+                    context.AddDefault(image);
                 }
 
-                context.AddDefault(book);
+                book = new Book(bestResult?.Title, bestResult?.Author, folder, image, false);
             }
+
+            return (book, true);
+        }
+
+        public DefaultResult<LookupBooksOutput> LookupBooks(UserContext context)
+        {
+            var audioFiles = context.Search<AudioFile>("FilePath", "", UserContext.ReadType.Shallow);
+            if (audioFiles == null) return null;
+            var folders = audioFiles.Select(d => d.Folder).Distinct().ToList();
+            var books = folders.Select(LookupBook).ToList();
 
             return null;
         }
 
-        
+
 
         private static List<AudioFile> FindAudioFiles(string path, bool shallow = false)
         {
